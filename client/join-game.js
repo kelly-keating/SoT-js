b8a9fdd2ff7f031462e053a1a22cf626760c98f6
kelly-keating <keating.kel@gmail.com>
2017-08-01 19:22:10 +1200

    join game filled

--- client/join-game.js
+++ client/join-game.js
@@ -7,8 +7,53 @@ class JoinGame extends React.Component {
             symbol: ''
         };
     }
+
+    handleNameChange (e) {
+        this.setState({ name: e.target.value });
+    }
+
+    handleSymbolChange (e) {
+        this.setState({ symbol: e.target.value });
+    }
+    joinGame (e) {
+        e.preventDefault();
+
+        fetch('/join-game', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then(response => response.json())
+        .then(data => this.props.onJoinGame(data.token));
+    }
     render () {
-        return <div></div>
+        return (
+            <form onSubmit={e => this.joinGame(e)}>
+                <h2>Join game:</h2>
+                <label>Name:</label>
+                <br/>
+                <input
+                  type="text"
+                  placeholder="Your name"
+                  value={this.state.name}
+                  onChange={e => this.handleNameChange(e)}
+                />
+                <br/>
+                <label>Symbol:</label>
+                <br/>
+                <input
+                  type="text"
+                  placeholder="X"
+                  value={this.state.symbol}
+                  onChange={e => this.handleSymbolChange(e)}
+                />
+                <br/>
+                <input type="submit" value="Join"/>
+            </form>
+        );
     }
 }
 window.JoinGame = JoinGame;
