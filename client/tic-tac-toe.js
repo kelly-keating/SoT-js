24026e7e0d31e65da292c55bf48736866dbddb96
kelly-keating <keating.kel@gmail.com>
2017-08-01 19:15:06 +1200

    tictac constructed

---
+++ client/tic-tac-toe.js
@@ -0,0 +1,15 @@
+class TicTacToe extends React.Component {
+  constructor () {
+      super();
+      this.state = {
+          gameState: {
+              players: []
+          },
+          token: null
+      };
+    }
+    render () {
+        return <div></div>
+    }
+}
+window.TicTacToe = TicTacToe;
