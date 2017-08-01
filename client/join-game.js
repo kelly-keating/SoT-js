26d273e9ab68c758c09f01ae1ae8533394bc472c
kelly-keating <keating.kel@gmail.com>
2017-08-01 19:20:02 +1200

    join game added

---
+++ client/join-game.js
@@ -0,0 +1,14 @@
+class JoinGame extends React.Component {
+    constructor (props) {
+        super();
+        this.props = props;
+        this.state = {
+            name: '',
+            symbol: ''
+        };
+    }
+    render () {
+        return <div></div>
+    }
+}
+window.JoinGame = JoinGame;
--- client/tic-tac-toe.js
+++ client/tic-tac-toe.js
@@ -8,8 +8,30 @@ class TicTacToe extends React.Component {
           token: null
       };
     }
+    componentDidMount () {
+        this.loadGameState();
+        setInterval(() => this.loadGameState(), 1000);
+    }
+    loadGameState () {
+        fetch('/game-state')
+        .then(response => response.json())
+        .then(gameState => this.setState({ gameState }));
+    }
+    setToken (token) {
+        this.state.token = token;
+    }
     render () {
-        return <div></div>
+        let { gameState, token } = this.state;
+
+        let gameIsEmpty = gameState.players.length === 0;
+        let waitingForPlayer = gameState.players.length === 1;
+        let gameIsFull = gameState.players.length === 2;
+        let gameIsUnderway = gameIsFull && gameState.whoseTurn;
+        let gameIsWon = gameIsFull && !gameState.whoseTurn && gameState.winner;
+        let gameIsDrawn = gameIsFull && !gameState.whoseTurn && !gameState.winner;
+        if (!token && !gameIsFull) {
+            return <JoinGame onJoinGame={token => this.setToken(token)}/>
+        }
     }
 }
 window.TicTacToe = TicTacToe;
